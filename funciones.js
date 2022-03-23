7aa2828b07844b7f0c1edc14b2b2d651eb437cc0
Alejandro Diaz <alejandro.diaz@axians.es>
2022-03-23 13:08:48 +0100

    add funciones.js

---
+++ funciones.js
@@ -0,0 +1,53 @@
+/*function mensajito(saludo, nombre){
+    saludo = '*' + saludo + '*';
+    alert(saludo + ", " + nombre);
+}
+
+mensajito("Buenas", "compañero");*/
+
+/*function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      return confirm('¿Tienes permiso de tus padres?');
+    }
+  }
+
+
+  function checkAge(age) {
+    return(age > 18) || confirm("Mensaje de permiso");
+  }
+*/
+
+/*let a = prompt("Introduce el primer numero.");
+let b = prompt("Introduce el segundo numero");
+
+function min(a, b) {
+    if (a < b) {
+        alert(a);
+    }else{
+        alert(b);
+    }
+}
+min(a, b);*/
+
+/*let x = prompt("Introduce el numero." + "");
+let n = prompt("introduce la potencia." + "");
+
+function pow(x, n) {
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+if (n < 1) {
+    alert(`Potencia ${n} no soportada,
+      use un entero mayor a 0`);
+  } else {
+    alert( pow(x, n) );
+  }
+*/
+
+
