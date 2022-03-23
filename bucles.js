ef43f856455ec19b5b8f79818af1a970e7f2f12a
Alejandro Diaz <alejandro.diaz@axians.es>
2022-03-23 10:01:19 +0100

    Add bucles.js

---
+++ bucles.js
@@ -0,0 +1,23 @@
+/*let i = 0;
+while (i < 3) {
+  alert( i );
+  i++;
+}*/
+
+/*for (let i = 0; i < 10; i++){
+    if (i % 2 == 0) continue;
+    alert(i);
+}*/
+
+/*let i=0
+while (i < 3) {
+    alert( `numero ${i++}!` );
+  }*/
+
+
+/*let number 
+
+do {
+    number = prompt("Ingresa un numero superior a 100:", 0);
+}while (number <= 100 && number);
+*/
