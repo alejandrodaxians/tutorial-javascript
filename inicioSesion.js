106b020f9de44855f9c20dbdae5c4a66c6d4c508
Alejandro Diaz <alejandro.diaz@axians.es>
2022-03-23 08:52:59 +0100

    Primera version

---
+++ inicioSesion.js
@@ -0,0 +1,15 @@
+let inicio = prompt ("Nombre de usuario:");
+
+if (inicio == "Admin"){
+    let password = prompt("Introduzca contraseña:");
+    
+    if (password == "TheMaster"){
+        alert("¡Bienvenido!");
+    }else if (password === null){
+        alert("Contraseña incorrecta");
+    }else if (password != "TheMaster"){
+        alert("Cancelado");
+    }
+}else if (inicio != "Admin") {
+    alert("No te conozco.");
+}
