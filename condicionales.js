9df56a1b494614b287a23a52abedd5b05fdff4e1
Alejandro Diaz <alejandro.diaz@axians.es>
2022-03-23 08:52:59 +0100

    Add all files

---
+++ condicionales.js
@@ -0,0 +1,41 @@
+/*let year = prompt('What is Jean Valjean prison number?', '');
+if (year == 24601) {
+    alert( '¡True!' );
+    alert("Very good!");
+} else {
+    alert("Error!");
+}
+*/
+
+/*let age = prompt('¿edad?', 18);
+
+if (age < 3) {
+    message = '¡Hola, bebé!';
+  } else if (age < 18) {
+    message = '¡Hola!';
+  } else if (age < 100) {
+    message = '¡Felicidades!';
+  } else {
+    message = '¡Qué edad tan inusual!';
+  }
+alert(message);
+*/
+
+/*let respuesta = prompt( "¿Nombre original de JavaScript", "");
+
+if (respuesta == "ECMAScript") {
+    alert("Correcto")
+}else{
+    alert("Error")
+}
+*/
+
+let numero = prompt ("Escribe un numero:");
+
+if (numero > 0) {
+    alert(1);
+}else if (numero == 0){
+    alert(0)
+}else{
+    alert(-1);
+}
