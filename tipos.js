cd57372f7d3be9e6c53ae842efcef160afa27dff
Alejandro Diaz <alejandro.diaz@axians.es>
2022-03-23 17:00:11 +0100

    Mañana sigo con tipos

---
+++ tipos.js
@@ -0,0 +1,8 @@
+/*let a = +prompt("Primer numero a sumar");
+let b = +prompt("Segundo numero a sumar");
+
+alert(a +b);*/
+
+/*function random(min, max) {
+    return min + Math.random() * (max - min);
+}*/
