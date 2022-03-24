893ec47f8b1ef0e93997e57a781f70c2520cd174
Alejandro Diaz <alejandro.diaz@axians.es>
2022-03-24 15:58:53 +0100

    Mañana seguimos con avanzado

---
+++ avanzado.js
@@ -0,0 +1,40 @@
+/*function fib(n){
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}*/
+
+//alert ( fib(77) );
+
+/*let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+  
+  function printList(list) {
+    let tmp = list;
+  
+    while (tmp) {
+      alert(tmp.value);
+      tmp = tmp.next;
+    }
+  
+  }
+  
+  printList(list);*/
+
+  
