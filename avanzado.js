feb4e01ec661d4840433cab9815e1ba94f8f04df
Alejandro Diaz <alejandro.diaz@axians.es>
2022-03-25 10:14:28 +0100

    Add avanzado.js

--- avanzado.js
+++ avanzado.js
@@ -37,4 +37,26 @@
   
   printList(list);*/
 
+ /*function inBetween(a, b) {
+  return function(x) {
+    return x >= a && x <= b; 
+  };
+ }
+
+ function inArrary([]) {
+  return function(x) {
+    return arr.includes(x);
+  };
+ }
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+alert( arr.filter(inBetween(3, 6)) ); 
   
+alert( arr.filter(inArray([1, 2, 10])) );*/
+
+/*function sayHi() {
+  alert('Han pasado cinco segundos');
+}
+
+setTimeout(sayHi, 5000);*/
